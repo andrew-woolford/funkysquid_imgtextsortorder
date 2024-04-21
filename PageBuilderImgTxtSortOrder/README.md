be64070011271928fdad8df70be73c70ff0974b2
Andy Woolford <162442964+andrew-woolford@users.noreply.github.com>
2024-04-21 20:50:48 +0100

    Initial module commit.

---
+++ PageBuilderImgTxtSortOrder/README.md
@@ -0,0 +1,3 @@
+# FunkySquid_PageBuilderImgTxtSort
+
+Page Builder content type for FunkySquid_PageBuilderImgTxtSort.
---
+++ PageBuilderImgTxtSortOrder/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Funky Squid Labs Ltd. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'FunkySquid_PageBuilderImgTxtSortOrder', __DIR__);
